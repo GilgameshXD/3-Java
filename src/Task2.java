2eb54b93d76591d6e5e13c67a001cd4cedab94a7
GilgameshXD <128893989+GilgameshXD@users.noreply.github.com>
2023-05-06 20:20:36 +0500

    Update Task2.java

--- src/Task2.java
+++ src/Task2.java
@@ -2,15 +2,15 @@ public class Task2 {
     public static void main(String[] args) {
 
         int CurrentBalance = 100;                   // Объявляете переменные для входных данных и
-        int TopupAmount = 200;                     // параметров программы: начального счёта,
+        int TopupAmount = 200;                      // параметров программы: начального счёта,
         int MinimumLimitForBonus = 1000;            // суммы пополнения и тп
         int NewBalance;
         int Bonuses = 0;
 
-        if (TopupAmount > MinimumLimitForBonus) {                                                                // Условным оператором проверяете, превысила ли
+        if (TopupAmount > MinimumLimitForBonus) {                                                            // Условным оператором проверяете, превысила ли
             NewBalance = CurrentBalance + TopupAmount + TopupAmount / 100;                                   // сумма пополнения порог, и для этих двух разных
             Bonuses = TopupAmount / 100;                                                                     // сценариев рассчитываете сумму бонуса и выводите
-        } else {                                                                                                 // на экран.
+        } else {                                                                                             // на экран.
             NewBalance = CurrentBalance + TopupAmount;
         }
         System.out.println("Ваш баланс: " + NewBalance + " рубль(-ей)" + "\nБонусов зачислено: " + Bonuses);
